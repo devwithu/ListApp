bb45749f9e26aba6a10c96b2532d5df59e333f3d
pdichone <pdichone@gmail.com>
2019-10-01 18:50:15 -0700

    Initial Commit

---
+++ ListApp-XC11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListApp-XC11
+//
+//  Created by Paulo Dichone on 10/1/19.
+//  Copyright © 2019 Paulo Dichone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
